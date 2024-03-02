d3b030516799e05aa8936ebbf79717628824aee7
CodeK254 <codekarma1003@gmail.com>
2024-03-02 21:32:54 +0300

    Commit 0.0.1

--- lib/services/import.dart
+++ lib/services/import.dart
@@ -1,26 +1,26 @@
-import 'dart:io';
+// import 'dart:io';
 
-import 'package:get/get.dart';
-import "package:file_picker/file_picker.dart";
+// import 'package:get/get.dart';
+// import "package:file_picker/file_picker.dart";
 
-class ImportController extends GetxController{
-  static File? picked;
-  // ImagePicker picker = ImagePicker();
+// class ImportController extends GetxController{
+//   static File? picked;
+//   // ImagePicker picker = ImagePicker();
 
-  static Future<File?> loadFile() async {
-    FilePickerResult? pickedFile = await FilePicker.platform.pickFiles(
-      type: FileType.custom,
-      allowedExtensions: ["txt"],
-    );
+//   static Future<File?> loadFile() async {
+//     FilePickerResult? pickedFile = await FilePicker.platform.pickFiles(
+//       type: FileType.custom,
+//       allowedExtensions: ["txt"],
+//     );
 
-    if(pickedFile != null){
-      picked = File(pickedFile.files.single.path!);
-    }
-    return picked;
-  }
+//     if(pickedFile != null){
+//       picked = File(pickedFile.files.single.path!);
+//     }
+//     return picked;
+//   }
 
-  static List setChats(File file, List data){
-    data = file.readAsString() as List;
-    return data;
-  }
-}
+//   static List setChats(File file, List data){
+//     data = file.readAsString() as List;
+//     return data;
+//   }
+// }
